2f2b293d8c5c8950e409df2fc09df77ef1746ef0
Ayusha200333 <ayushawijerathna@gmail.com>
2025-06-16 14:51:16 +0530

    add delete complaint servlet

--- src/main/java/org/example/cmsjavaee/controller/SignupServlet.java
+++ src/main/java/org/example/cmsjavaee/controller/SignupServlet.java
@@ -1,21 +1,28 @@
 package org.example.cmsjavaee.controller;
 
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.example.cmsjavaee.dto.UserDto;
+import org.example.cmsjavaee.model.UserModel;
 
 import java.io.IOException;
 
 @WebServlet("/signup")
 public class SignupServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String name = req.getParameter("name");
         String password = req.getParameter("password");
         String email = req.getParameter("email");
 
-        System.out.println(name + " " + password + " " + email);
+        ServletContext servletContext = req.getServletContext();
+//        System.out.println(name + " " + password + " " + email);
+        if (UserModel.createUser(new UserDto(name,email,password,"employee"),servletContext)){
+            resp.sendRedirect(req.getContextPath()+"/Employee.jsp");
+        }
     }
 }
