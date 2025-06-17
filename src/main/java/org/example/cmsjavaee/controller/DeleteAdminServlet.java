c39c00f9778d8e42ea53f838dd6f5c5d60357e35
Ayusha200333 <ayushawijerathna@gmail.com>
2025-06-17 11:47:08 +0530

    add delete admin servlet

---
+++ src/main/java/org/example/cmsjavaee/controller/DeleteAdminServlet.java
@@ -0,0 +1,9 @@
+package org.example.cmsjavaee.controller;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet("/adminDelete")
+public class DeleteAdminServlet extends HttpServlet {
+
+}
